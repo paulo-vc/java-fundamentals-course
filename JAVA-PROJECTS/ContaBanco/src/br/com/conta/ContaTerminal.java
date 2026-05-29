ae0c2204860527c9e31af9a8ff3ad07cb2b3e142
paulo-vc <contato.paulovruffini@gmail.com>
2026-05-29 16:24:45 -0300

    desafio dio conta bancária

---
+++ JAVA-PROJECTS/ContaBanco/src/br/com/conta/ContaTerminal.java
@@ -0,0 +1,15 @@
+package br.com.conta;
+
+public class ContaTerminal {
+    private int idConta;
+    private String agenciaConta;
+    private String nomeUsuario;
+    private double saldoDisponivel;
+
+    public void criarConta(String idConta){
+        
+    }
+
+}
+
+
